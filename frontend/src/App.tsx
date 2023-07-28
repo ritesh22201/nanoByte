1f07c3225210ac1b411b74ba070e5b03d208048c
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-28 23:08:59 +0530

    code added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import MainRoutes from './Routes/MainRoutes';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <MainRoutes/>
+     {/* <Footer/> */}
     </div>
   );
 }
